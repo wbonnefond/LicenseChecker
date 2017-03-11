f199cc1f324be5d48f039bd877dd03b2cefdf292
Ward Bonnefond <wbonnefond@alarm.com>
2017-03-11 14:09:30 -0500

    Initial commit for LicenseChecker

---
+++ licenseChecker/src/main/groovy/com/wardbonnefond/licensechecker/LicenseChecker.groovy
@@ -0,0 +1,32 @@
+package com.wardbonnefond.licensechecker
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LicenseChecker implements Plugin<Project> {
+    void apply(Project project) {
+        project.extensions.create('licenseChecker', LicenseCheckerExtension)
+
+        def variants = null
+        if (project.android.hasProperty('applicationVariants')) {
+            variants = project.android.applicationVariants
+        }
+        else if (project.android.hasProperty('libraryVariants')) {
+            variants = project.android.libraryVariants
+        }
+        else {
+            throw new IllegalStateException('Android project must have applicationVariants or libraryVariants!')
+        }
+
+        project.afterEvaluate {
+            AttributionGenerationTask checkerTask = project.task("generateLicenseAttributions", type: AttributionGenerationTask)
+            checkerTask.outputFile = new File(project.projectDir, project.licenseChecker.outputFolder + "/"+ project.licenseChecker.outputFileName)
+            println(checkerTask.outputFile.absolutePath)
+            println(project.licenseChecker.inputFile)
+            println(project.licenseChecker.outputFileName)
+
+            project.tasks.preBuild.dependsOn(checkerTask)
+        }
+
+    }
+}
---
+++ licenseChecker/src/main/groovy/com/wardbonnefond/licensechecker/LicenseCheckerExtension.groovy
@@ -0,0 +1,16 @@
+package com.wardbonnefond.licensechecker
+
+/**
+ * Configuration values for the victor {} script block.
+ */
+class LicenseCheckerExtension {
+
+    private final static DEFAULT_OUTPUT_FOLDER = 'src/main/assets'
+    private final static DEFAULT_INPUT_FILE = 'licenses.json'
+
+    def outputFolder = 'src/main/assets'
+
+    def outputFileName = "open_source_licenses.html"
+
+    def inputFile = 'licenses.json'
+}
