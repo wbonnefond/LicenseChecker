5f121b228cbdfd13cf8c070de1beed1a08900aa9
Ward Bonnefond <wbonnefond@gmail.com>
2017-03-13 07:39:39 -0400

    Move resources to resources folders

--- license-checker/src/main/groovy/com/wardbonnefond/licensechecker/AttributionGenerationTask.groovy
+++ license-checker/src/main/groovy/com/wardbonnefond/licensechecker/AttributionGenerationTask.groovy
@@ -55,9 +55,9 @@ class AttributionGenerationTask extends DefaultTask {
         Utils.ensureAllDependenciesAccountedFor(dependenciesMap, logger, failOnMissingAttributions)
 
         // build the HTML file
-        String finalHtml = new File(project.parent.projectDir, "/license-checker/base-html").text
+        String finalHtml = new File(project.parent.projectDir, "/license-checker/src/main/resources/base-html").text
 
-        String html = new File(project.parent.projectDir, "/license-checker/individual-license-html").text
+        String html = new File(project.parent.projectDir, "/license-checker/src/main/resources/individual-license-html").text
         StringBuilder sb = new StringBuilder();
 
         configParser.jsonConfig.licenses.each { k ->
